127bc80c252952b87568305d7956fc8af9b1b895
Dt-Luna <luna.deodato@escolar.ifrn.edu.br>
2025-02-19 08:52:51 -0300

    criação de sprites

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ class playerSprite(pygame.sprite.Sprite):
     def __init__(self):
          super().__init__()
          macaco_img = pygame.image.load('macaco.png').convert_alpha()
-         macaco_img = pygame.transform.scale(macaco_img, (50,50))
+         macaco_img = pygame.transform.scale(macaco_img, (150,150))
          self.image = macaco_img
          self.rect = macaco_img.get_rect()
          self.rect.topleft = (320,120)
@@ -14,7 +14,21 @@ class playerSprite(pygame.sprite.Sprite):
 
     def tick(self):
         self.rect.y += self.vertical_speed
-        # self.vertical_speed += 0.1
+        self.vertical_speed += 0.1
+
+class blocosSprite(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        tronco_img = pygame.image.load('tronco.png').convert_alpha()
+        tronco_img = pygame.transform.scale(tronco_img, (300,300))
+        self.image = tronco_img
+        self.rect = tronco_img.get_rect()
+        self.rect.topleft = (x, y)
+
+class serpentesSprite(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__():
+        
 
 pygame.init()
 screen = pygame.display.set_mode((640,450))
@@ -22,7 +36,8 @@ clock = pygame.time.Clock()
 running = True
 
 player = playerSprite()
-todos_sprites = pygame.sprite.Group([player])
+blocos = [blocosSprite(2+i*10, 40+i*100)for i in range(5)]
+todos_sprites = pygame.sprite.Group([player, blocos])
 
 while running:
     for event in pygame.event.get():
